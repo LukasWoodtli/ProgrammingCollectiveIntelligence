939ad125f17d73ab40c208549db9995e52c351f2
woodtluk <woodtluk@e17a30e0-a4a0-48a1-9280-7d2f855b2862>
2011-08-16 19:07:48 +0000

    Diverse Programmier Projekte (Python, Lisp) angefangen.
    git-svn-id: https://xp-dev.com/svn/woodtluk-myStuff@232 e17a30e0-a4a0-48a1-9280-7d2f855b2862

---
+++ Chapter 03/generatefeedvector.py
@@ -0,0 +1,80 @@
+'''
+Created on 13.08.2011
+
+@author: luki
+'''
+
+import feedparser
+import re
+
+def getwords(html):
+    # Remove all the HTML tags
+    txt=re.compile(r'<[^>]+>').sub('',html)
+    # Split words by all non-alpha characters
+    words=re.compile(r'[^A-Z^a-z]+').split(txt)
+    # Convert to lower case
+    return [word.lower() for word in words if word!='']
+
+# Returns title and dictionary of word counts for an RSS feed
+def getwordcounts(url):
+    # Parse the feed
+    d=feedparser.parse(url)
+    wc={}
+    # Loop over all the entries
+    for e in d.entries:
+        if 'summary' in e:
+            summary=e.summary
+        else:
+            summary=e.description
+
+        # Extract a list of words
+        words=getwords(e.title+' '+summary)
+        for word in words:
+            wc.setdefault(word,0)
+            wc[word]+=1
+
+    title = ''
+    try:
+        title = d.feed.title
+    except:
+        title = 'No title'
+    
+    return title,wc
+    
+
+apcount = {}
+wordcounts = {}
+feedlist = [line for line in file('feedlist.txt')]
+for feedurl in feedlist:
+    try:
+        title,wc = getwordcounts(feedurl)
+        wordcounts[title] = wc
+        for word, count in wc.items():
+            apcount.setdefault(word, 0)
+            if count > 0:
+                apcount[word] += 1
+    except:
+        print 'Failed to parse feed %s' % feedurl
+        
+                    
+
+wordlist = []
+for w, bc in apcount.items():
+    frac =  float(bc)/len(feedlist)
+    if frac > 0.1 and frac < 0.5:
+        wordlist.append(w)
+    
+out = file('blogdata.txt', 'w')
+out.write('Blog')
+for word in wordlist:
+    out.write('\t%s' % word)
+out.write('\n')
+
+for blog, wc in wordcounts.items():
+    out.write(blog)
+    for word in wordlist:
+        if word in wc:
+            out.write('\t%d' % wc[word])
+        else:
+            out.write('\t0')
+    out.write('\n')
